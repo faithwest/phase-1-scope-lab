c0de0e65b088ffb22cc838d5bb4efb9fe6498dc3
faithwest <faithchepkoech425@gmail.com>
2023-09-21 20:35:28 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+var bestCustomer;
+
+function setBestCustomer() {
+  bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = "maybe bob";
+}
+
+const LeastFavoriteCustomer = "betty";
+
+
+function changeLeastFavoriteCustomer() {
+  LeastFavoriteCustomer = "jane";
+}
 // Write your solution in this file!
